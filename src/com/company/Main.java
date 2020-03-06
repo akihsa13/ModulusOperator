ff201b8e4463c7ffff3c03c742d70b20eb444fef
Ashika <ashika.baraili.2023@student.lhschools.org>
2020-03-06 14:06:17 -0500

    keepContainedMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,60 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	testOddness();
+	keepContained();
+
+
+
+
+
     }
+
+    public static void testOddness() {
+// testOddness
+        //Number is odd:
+
+        // we want to find if a number is even or odd
+        Scanner keyboard;
+        System.out.println("give me a positive number");
+
+        keyboard = new Scanner (System.in);
+       int number = keyboard.nextInt();
+
+
+        System.out.println("Number is odd : "  + (number % 2!=0));
+
+    }
+
+    public static void keepContained() {
+
+
+        Scanner keyboard;
+        keyboard = new Scanner (System.in);
+        System.out.println("Insert a positive number");
+        int number= keyboard.nextInt();
+        System.out.println(number+" contained is:"+((number% 8)+5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
